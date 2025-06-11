b6084ca84472d05c9da2bc8a9d95141d486e6f7a
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-06-11 19:32:59 -0400

    react

---
+++ app/welcome/components/NavBar.tsx
@@ -0,0 +1,15 @@
+export function NavBar() {
+  return (
+    <div className="container base">
+        <div className="rows row-4">
+           
+                <a href="#home" className="nav-btn">Home</a>
+                <a href="#experiences" className="nav-btn">Experiences</a>
+                <a href="#projects" className="nav-btn">Projects</a>
+                <a href="#contact" className="nav-btn">Contact</a>
+                
+        </div>
+    </div>
+  );
+}
+
