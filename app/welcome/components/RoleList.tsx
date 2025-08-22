4e9abfea5f2951273326216765f54a3dd969000a
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-08-21 23:10:27 -0400

    effects on hover

--- app/welcome/components/RoleList.tsx
+++ app/welcome/components/RoleList.tsx
@@ -12,11 +12,11 @@ type Props = {
 
 export function RoleList({ articles }: Props) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 hover">
       {articles.map((role) => (
         <div
           key={role.id}
-          className="p-4 text-[#FEFAE0] bg-[#3b4e2f] rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+          className="p-4 text-[#FEFAE0] bg-[#3b4e2f] rounded-lg shadow-md hover:bg-[#6a8a55] transition-shadow duration-300"
         >
           <div className="flex justify-between items-center mb-2">
             <h2 className="font-semibold text-md">{role.title}</h2>
